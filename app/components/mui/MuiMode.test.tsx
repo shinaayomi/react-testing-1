dd24088e9f78524a4ea6e686d27fd8ee8b72df51
shinaayomi <lateef718@gmail.com>
2025-09-16 05:15:42 +0100

    provider test

---
+++ app/components/mui/MuiMode.test.tsx
@@ -0,0 +1,11 @@
+import { render, screen } from "../../test-utils";
+import { MuiMode } from "./MuiMode";
+
+// TESTING ON PROVIDERS
+describe("MuiMode", () => {
+  test("renders text correctly", () => {
+    render(<MuiMode />);
+    const headingElement = screen.getByRole("heading");
+    expect(headingElement).toHaveTextContent("dark mode");
+  });
+});
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,6 +3,7 @@ import { Welcome } from "../welcome/welcome";
 import { Application } from "../components/application/Application";
 import { Skills } from "~/components/skills/Skills";
 import { Counter } from "~/components/counter/Counter";
+import { MuiMode } from "~/components/mui/MuiMode";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -14,10 +15,19 @@ export function meta({}: Route.MetaArgs) {
 export default function Home() {
   // return <Welcome />;
   return (
-    <div>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        gap: "px",
+        alignItems: "center",
+        paddingTop: "20px",
+      }}
+    >
       <Application />
       <Skills skills={["HTML", "CSS"]} />
       <Counter />
+      <MuiMode />
     </div>
   );
 }
