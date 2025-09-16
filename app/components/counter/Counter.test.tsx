adce634226454689b8e85fa87eab0b9cda2b9ecb
shinaayomi <lateef718@gmail.com>
2025-09-16 04:39:55 +0100

    Completed count testing

--- app/components/counter/Counter.test.tsx
+++ app/components/counter/Counter.test.tsx
@@ -17,10 +17,9 @@ describe("Counter", () => {
     const countElement = screen.getByRole("heading");
     expect(countElement).toHaveTextContent("0");
   });
-
+  // EVENT INTERACTION
   test("render a count of 1 aftrer clicking the increment button", async () => {
     user.setup();
-
     render(<Counter />);
     const incrementButton = screen.getByRole("button", { name: "Increment" });
     await user.click(incrementButton);
@@ -30,11 +29,36 @@ describe("Counter", () => {
 
   test("render a count of 2 after clicking the increment button twice", async () => {
     user.setup();
-
     render(<Counter />);
     const incrementButton = screen.getByRole("button", { name: "Increment" });
     await user.dblClick(incrementButton);
     const countElement = screen.getByRole("heading");
     expect(countElement).toHaveTextContent("2");
+  });
+
+  test("render a count of 10 after clicking the set button", async () => {
+    user.setup();
+    render(<Counter />);
+    const amountInput = screen.getByRole("spinbutton");
+    await user.type(amountInput, "10");
+    expect(amountInput).toHaveValue(10);
+    const setButton = screen.getByRole("button", { name: "Set" });
+    await user.click(setButton);
+    const countElement = screen.getByRole("heading");
+    expect(countElement).toHaveTextContent("10");
+  });
+  // TO TEST KEYBOARD INTERACTION
+  test("elements are focused in the right order when tabbing", async () => {
+    user.setup();
+    render(<Counter />);
+    const amountInput = screen.getByRole("spinbutton");
+    const setButton = screen.getByRole("button", { name: "Set" });
+    const incrementButton = screen.getByRole("button", { name: "Increment" });
+    await user.tab();
+    expect(incrementButton).toHaveFocus();
+    await user.tab();
+    expect(amountInput).toHaveFocus();
+    await user.tab();
+    expect(setButton).toHaveFocus();
   });
 });
